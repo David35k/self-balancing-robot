d978f1a907a439dc9ab601971714247bdfe67a9a
David <davidtoman07@gmail.com>
2023-09-25 19:44:51 +1300

    Initial commit

---
+++ test/test.ino
@@ -0,0 +1,13 @@
+//includes
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("wassup");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
